3da25f74c6eca16946054ac2ba70b4d46e0d78d5
MahirMahdi <mahdimahir45@gmail.com>
2023-08-18 21:36:39 +0600

    added custom mount and isInViewport command

---
+++ client/cypress/support/commands.jsx
@@ -0,0 +1,52 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { mount } from "cypress/react";
+import { MemoryRouter } from "react-router-dom";
+import { customTheme } from "../../src/utils/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+
+Cypress.Commands.add("mount", (component, options = {}) => {
+  const { routerProps = { initialEntries: ["/"] }, ...mountOptions } = options;
+
+  const wrapped = (
+    <MemoryRouter {...routerProps}>
+      <ChakraProvider theme={customTheme}>{component}</ChakraProvider>
+    </MemoryRouter>
+  );
+
+  return mount(wrapped, mountOptions);
+});
+
+Cypress.Commands.add("isInViewport", { prevSubject: true }, (subject) => {
+  const bottom = Cypress.$(cy.state("window")).height();
+  const rect = subject[0].getBoundingClientRect();
+
+  expect(rect.top).not.to.be.greaterThan(bottom);
+  expect(rect.bottom).not.to.be.greaterThan(bottom);
+
+  return subject;
+});
