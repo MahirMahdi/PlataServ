37658a37d577a9e0c0c127316dc47df3028ced03
MahirMahdi <mahdimahir45@gmail.com>
2023-08-18 21:41:49 +0600

    installed and configured cypress-vite

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: "http://localhost:5173",
+    setupNodeEvents(on) {
+      on("file:preprocessor", vitePreprocessor());
+    },
   },
 
   component: {
